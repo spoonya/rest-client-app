9aca713487a180521ee29b638b9c3ac3536bf33b
Yuliya Ivashchanka <jjjuliettai@gmail.com>
2025-04-02 20:05:36 +0300

    feat: add condition to display menu

--- src/app/[locale]/(root)/page.tsx
+++ src/app/[locale]/(root)/page.tsx
@@ -2,13 +2,14 @@
 
 import { useTranslations } from 'next-intl';
 
-import { PageLayout } from '@/components';
+import { Menu, PageLayout } from '@/components';
 
 export default function Home() {
   const t = useTranslations('HomePage');
 
   return (
     <PageLayout title={t('title')}>
+         <Menu/>
       <p className="max-w-[590px]">{t.rich('description')}</p>
     </PageLayout>
   );
