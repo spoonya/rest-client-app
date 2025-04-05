6dfacc0df31d508afd644a3cf04fdeb59bbc26f1
Tsimafei <xsetiqq@gmail.com>
2025-04-05 20:45:33 +0200

    add: code generation with headers and body taken into account and that headers and body themselves are taken into account in the request

--- src/app/[locale]/(root)/page.tsx
+++ src/app/[locale]/(root)/page.tsx
@@ -2,7 +2,7 @@
 
 import { useTranslations } from 'next-intl';
 
-import { AuthButtons, Menu, PageLayout } from '@/components';
+import { Menu, PageLayout } from '@/components';
 import { useAuth } from '@/hooks';
 
 import { AuthButtons } from '@/components/shared/AuthButtons';
@@ -16,43 +16,39 @@ export default function Home() {
   const user = useAuth();
   const name = user?.user_metadata.full_name;
   const [isLoading, setIsLoading] = useState(false);
-    const router = useRouter();
-
+  const router = useRouter();
 
   useEffect(() => {
-      const checkAuth = async () => {
-        setIsLoading(true)
-        supabase.auth.getUser().finally(()=> setIsLoading(false))
-  
-      };
-  
-      checkAuth();
-    }, [router]);
-  
-    if (isLoading) {
-      return (
-        <div className="flex justify-center items-center h-screen">
-          <Image
-            src="/loaders/Loader1.svg"
-            alt="Loading..."
-            width={100}
-            height={100}
-          />
-        </div>
-      );
-    }
-  
+    const checkAuth = async () => {
+      setIsLoading(true);
+      supabase.auth.getUser().finally(() => setIsLoading(false));
+    };
 
-   
+    checkAuth();
+  }, [router]);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Image
+          src="/loaders/Loader1.svg"
+          alt="Loading..."
+          width={100}
+          height={100}
+        />
+      </div>
+    );
+  }
+
   return (
     <PageLayout title={t('title')} userName={name}>
-
-      <div className='w-full shadow-lg flex items-center justify-around py-10'>
-
-         {( user)? (<Menu/>):
-         (<AuthButtons className='z-0 rounded-2xl text-blue-800'/>)}
+      <div className="w-full shadow-lg flex items-center justify-around py-10">
+        {user ? (
+          <Menu />
+        ) : (
+          <AuthButtons className="z-0 rounded-2xl text-blue-800" />
+        )}
       </div>
-
     </PageLayout>
   );
 }
