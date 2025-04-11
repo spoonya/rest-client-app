be532144fbfbe4921d1f69ac108e8a305640178a
Yuliya Ivashchanka <jjjuliettai@gmail.com>
2025-04-11 11:03:20 +0300

    feat: add translation to history page

--- src/app/[locale]/(protected)/history/page.tsx
+++ src/app/[locale]/(protected)/history/page.tsx
@@ -4,12 +4,14 @@ import { AppRoutes } from '@/services';
 import { HttpObject, RequestObject } from '@/types';
 import { cn } from '@/utils';
 import { Link } from '@heroui/react';
+import { useTranslations } from 'next-intl';
 
 interface HistoryProps {
   className?: string;
 }
 
 export default function History({className}: HistoryProps) {
+  const t = useTranslations('History');
   const requestKeys = localStorage.getItem('requestKeys')?.split(',') || []; 
   const requestList: HttpObject[] | [] = requestKeys?.map((item) => {
     const request: RequestObject = JSON.parse(localStorage.getItem(item) || '');
@@ -19,12 +21,12 @@ export default function History({className}: HistoryProps) {
   return (
     <div className={cn(className, "flex p-4 overflow-hidden")}>
  {!requestKeys.length ? <div>
-  You haven&lsquo;t executed any requests. It&lsquo;s empty here. Try:
+  {t('empty')}
   <Link href={AppRoutes.REST}>Rest Client</Link>
   </div>: 
             <div className="flex flex-col h-auto justify-between items-center w-full">
               <h2 className="text-lg mb-4 font-semibold text-foreground/80">
-                History requests
+              {t('title')}
               </h2>
               <HistoryList list={listSort}/>
               </div>}
